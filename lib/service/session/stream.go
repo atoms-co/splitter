23fa92467d90102016cca3d40e2a1f0f8ce7d255
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-10-20 11:07:32 -0700

    [libs/go] Rename chan.Append to Prepend and AppendLast to Append
    Less words.
    
    GitOrigin-RevId: 173ffbf6919d67d0a8abf6b82902b012faa56cab

--- lib/service/session/stream.go
+++ lib/service/session/stream.go
@@ -18,7 +18,7 @@ func Connect[T any](c *Client, establish Message, main <-chan T, liveness <-chan
 // Receive creates a server-side session output for a message stream of type T, terminated with Closed.
 // Terminates the session if the main chan is closed.
 func Receive[T any](s *Server, main <-chan T, liveness <-chan Message, inject func(Message) T) <-chan T {
-	return chanx.AppendLast(
+	return chanx.Append(
 		chanx.Join(chanx.Breaker(main, s, serverBufChanSize), chanx.Map(liveness, inject)),
 		inject(NewClosedMessage()),
 	)
